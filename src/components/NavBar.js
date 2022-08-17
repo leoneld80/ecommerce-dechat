8a95c76b88cbee63f5affbf3c32aa9600292190a
leoneld80 <leoneld80@gmail.com>
2022-08-17 12:22:46 -0300

    arreglo href

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
     <>
     <nav className="navbar navbar-expand-sm navbar-light bg-light">
         <div className="container">
-        <a className="navbar-brand" href="#">E-commerce</a>
+        <a className="navbar-brand" href="/">E-commerce</a>
         <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
           aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -13,16 +13,16 @@ function NavBar() {
         <div className="collapse navbar-collapse" id="collapsibleNavId">
           <ul className="navbar-nav me-auto mt-2 mt-lg-0">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="visually-hidden">(current)</span></a>
+              <a className="nav-link" href="/">Home <span className="visually-hidden">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Ofertas</a>
+              <a className="nav-link" href="/">Ofertas</a>
             </li>
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categorias</a>
+              <a className="nav-link dropdown-toggle" href="/" id="dropdownId" data-bs-toggle="dropdown" aria-expanded="false">Categorias</a>
               <div className="dropdown-menu" aria-labelledby="dropdownId">
-                <a className="dropdown-item" href="#">Categoria 1</a>
-                <a className="dropdown-item" href="#">Categoria 2</a>
+                <a className="dropdown-item" href="/">Categoria 1</a>
+                <a className="dropdown-item" href="/">Categoria 2</a>
               </div>
             </li>
           </ul>
