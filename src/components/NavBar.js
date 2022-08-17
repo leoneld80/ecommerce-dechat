b1f80c7e6d68ed3773492a5b235bddc71f28ad4e
leoneld80 <leoneld80@gmail.com>
2022-08-17 13:39:28 -0300

    arreglo idioma, titulo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
     <>
     <nav className="navbar navbar-expand-sm navbar-light bg-light">
         <div className="container">
-        <a className="navbar-brand" href="/">E-commerce</a>
+        <a className="navbar-brand" href="/"><i class="bi bi-shop"></i> E-commerce</a>
         <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
           aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -13,7 +13,7 @@ function NavBar() {
         <div className="collapse navbar-collapse" id="collapsibleNavId">
           <ul className="navbar-nav me-auto mt-2 mt-lg-0">
             <li className="nav-item active">
-              <a className="nav-link" href="/">Home <span className="visually-hidden">(current)</span></a>
+              <a className="nav-link" href="/">Inicio <span className="visually-hidden">(current)</span></a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/">Ofertas</a>
@@ -27,8 +27,8 @@ function NavBar() {
             </li>
           </ul>
           <form className="d-flex my-2 my-lg-0">
-            <input className="form-control me-sm-2" type="text" placeholder="Search"/>
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <input className="form-control me-sm-2" type="text" placeholder="Buscar.."/>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
           </form>
         </div>
       </div>
